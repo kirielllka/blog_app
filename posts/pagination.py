6c7ca709695d40522d8f79d6c3e58947cab91cb8
Yan <korotkevich3007@gmail.com>
2024-09-26 18:31:31 +0300

    first

--- posts/pagination.py
+++ posts/pagination.py
@@ -7,3 +7,6 @@ class CommentApiPagination(PageNumberPagination):
     page_size = 2
     page_size_query_param = 'page_size'
     max_page_size = 1000
+
+class PostApiPagination(CommentApiPagination):
+    pass
